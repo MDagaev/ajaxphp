a8b7a048920e5f36bf4f9a4f147f99dfd6cbd7e4
MDagaev <tavintavan@gmail.com>
2018-05-20 19:22:06 +0800

    Передача данных из скрипта в  обработчик

--- test.php
+++ test.php
@@ -1,3 +1,10 @@
 <?php
 //var_dump($_GET);
-echo $_GET["a"] . ", " . $_GET["b"];
+//echo $_GET["title"] . ", " . $_GET["date"];
+    $arr['a'] = 10;
+    $arr['b'] = 20;
+    $arr['c'] = 30;
+
+echo json_encode($arr);
+
+
